d7de6b04b3da1f801fc86d60190ff55bade36a4b
Sass-1 <sassielouis5@gmail.com>
2026-01-13 18:28:16 -0500

    Progress of Lab04-2

--- Lab04-Scanner.cpp
+++ Lab04-Scanner.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>      
 
 using namespace std;
 
 int main() {
-    
+    // 1. Create socket
     int my_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (my_socket == -1) return 1;
+
+    // 2. Prepare target address
     struct sockaddr_in cible;
     cible.sin_family = AF_INET;
-    cible.sin_port = htons(80);
+    cible.sin_port = htons(80); 
     inet_pton(AF_INET, "127.0.0.1", &cible.sin_addr);
 
-    cout<<"L'adresse de la cible est prete !" <<endl;
+    // 3. Try to connect
+    cout << "Scanning port 80..." << endl;
+    int resultat = connect(my_socket, (struct sockaddr*)&cible, sizeof(cible));
 
+    // 4. Check result
+    if (resultat == 0) {
+        cout << "Port 80 is OPEN!" << endl;
+    } else {
+        cout << "Port 80 is CLOSED." << endl;
+    }
 
-
+    // 5. Cleanup
+    close(my_socket);
     return 0;
 }
